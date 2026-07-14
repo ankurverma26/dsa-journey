83c4a66bef44588b6d7738665a70b43e40a54912
Ankur Verma <ankur.2611verma@gmail.com>
2026-07-14 19:12:44 +0530

    Update solution

--- Arrays/stock_buy_sale.cpp
+++ Arrays/stock_buy_sale.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int profit(int arr[],int size){
     int maxprof=0,minprice=arr[0];
     for(int i=0;i<size;i++){
-        if(arr[i]-minprice<0) minprice=arr[i];
+        if(arr[i]<minprice) minprice=arr[i];
         if(arr[i]-minprice>maxprof) maxprof=arr[i]-minprice;
     }
     return maxprof;
