0e9218352d96ea787662a7a05e40b4a343f2c9a9
Ankur Verma <ankur.2611verma@gmail.com>
2026-07-14 19:12:08 +0530

    Add question

---
+++ Arrays/stock_buy_sale.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int profit(int arr[],int size){
+    int maxprof=0,minprice=arr[0];
+    for(int i=0;i<size;i++){
+        if(arr[i]-minprice<0) minprice=arr[i];
+        if(arr[i]-minprice>maxprof) maxprof=arr[i]-minprice;
+    }
+    return maxprof;
+}
+int main(){
+    int arr[]={7,1,5,3,6,4};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    cout<<profit(arr,size);
+    return 0;
+}
