1a0f0f9b047ccfcc71e5fdea2f4000a67a8dcf35
Ankur Verma <ankur.2611verma@gmail.com>
2026-07-14 23:53:43 +0530

    Optimal solution

--- Arrays/leader.cpp
+++ Arrays/leader.cpp
@@ -1,24 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// vector<int> leader(int arr[],int size){
+//     vector<int> temp;
+//     vector<int>ans;
+//     int i=size-1,current;
+//     while(i>=0){
+//         temp.push_back(arr[i]);
+//         current=arr[i];
+//         for(int j: temp){
+//             if(current<j){
+//                 current=0;
+//                 break;
+//             }
+//         }
+//         if(current==arr[i]) ans.push_back(current);
+//         i--;
+//     }
+//     return ans;
+// }
+
 vector<int> leader(int arr[],int size){
-    vector<int> temp;
-    vector<int>ans;
-    int i=size-1,current;
-    while(i>=0){
-        temp.push_back(arr[i]);
-        current=arr[i];
-        for(int j: temp){
-            if(current<j){
-                current=0;
-                break;
-            }
+    vector<int> ans;
+    int maxi=arr[size-1];
+    ans.push_back(maxi);
+    for(int i=size-2;i>=0;i--){
+        if(arr[i]>maxi){
+            ans.push_back(arr[i]);
+            maxi=arr[i];
         }
-        if(current==arr[i]) ans.push_back(current);
-        i--;
     }
+    reverse(ans.begin(),ans.end());
     return ans;
 }
+
+
+
 int main(){
     int arr[]={4, 7, 1, 0};
     int size=sizeof(arr)/sizeof(arr[0]);
