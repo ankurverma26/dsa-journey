3b9415729f8424f5856a673e877ea949a0c6cfb5
Ankur Verma <ankur.2611verma@gmail.com>
2026-09-02 23:29:24 +0530

    Add leetcode 137

---
+++ Arrays/singleNumberII.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    unordered_map<int, int> mp;
+    for (int i : nums)
+        mp[i]++;
+    for (auto it : mp)
+        if (it.second == 1)
+            return it.first;
+    return -1;
+}
+
+int main(){
+    vector<int>nums={2,2,3,2};
+    cout<<singleNumber(nums);
+    return 0;
+}
