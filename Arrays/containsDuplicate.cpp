9a643ab219e1986501711ce9548f0701834b80b5
Ankur Verma <ankur.2611verma@gmail.com>
2026-09-02 21:37:00 +0530

    Add leetcode 217

---
+++ Arrays/containsDuplicate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool containsDuplicate(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    vector<int> nums={1,2,3,1};
+    if(containsDuplicate(nums)) cout<<"True";
+    else cout<<"False";
+    return 0;
+}
