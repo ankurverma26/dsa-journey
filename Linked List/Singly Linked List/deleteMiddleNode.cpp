6bee2ec38bd9b2524c7225e49a9fe39b3993753b
Ankur Verma <ankur.2611verma@gmail.com>
2026-09-06 14:02:56 +0530

    Add leetcode 2095

---
+++ Linked List/Singly Linked List/deleteMiddleNode.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *deleteMiddle(ListNode *head)
+{
+    if (head->next == nullptr)
+        return nullptr;
+    ListNode *slow = head;
+    ListNode *fast = head;
+    ListNode *prev = head;
+    while (fast && fast->next)
+    {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+    delete slow;
+    return head;
+}
